63dd3700f892db69c227ca89c12460a7a371555e
Millisami <sachin@sprout-technology.com>
2010-10-30 00:25:54 +0545

    settings_path altered

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,8 +9,8 @@ class DashboardController < ApplicationController
       @today_contributors = []
       @top_contributors = []
       
-      if current_user.roles.include?("site_admin") && current_user.sign_in_count == 2 && current_account.users.count == 1
-        redirect_to edit_account_setting_path(current_account)
+      if current_user.roles && current_user.roles.include?("site_admin") && current_user.sign_in_count == 2 && current_account.users.count == 1
+        redirect_to settings_path
       end
   end
   
