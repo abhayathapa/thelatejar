b0ab87bab22c5c2b3e93bbc3b754879786a81d9f
neutrino <ranendra@sprout-technology.com>
2010-10-29 23:25:35 +0545

    added a days method and method to assigned a default value for a accounts with before_create

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,27 @@ class Account < ActiveRecord::Base
   
   validates_uniqueness_of :name, :case_sensitive => false
   validates_presence_of :name
+  
+  before_create :set_default_values
+
+  
+  def self.days 
+    {
+        "1" => "Monday" ,
+        "2" => "Tuesday" ,
+        "3" => "Wednesday" ,
+        "4" => "Thursday" ,
+        "5" => "Friday" ,
+        "6" => "Saturday",
+        "7" => "Sunday"
+    }
+  end
+  
+  private
+   def set_default_values
+     self.time_zone = 'Kathmandu'
+     self.office_closing_days = "6,7"
+     self.fine_amount = 15.0
+     self.late_after = 0
+   end
 end
