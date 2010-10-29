ee068ce6400546dbeaa1a1a72c3687482ef52fdc
zoras <sawrows@gmail.com>
2010-10-29 19:28:35 +0545

    After signup, Account admin is redirected to settings page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,10 @@ class DashboardController < ApplicationController
       @raised_amount = 0
       @today_contributors = []
       @top_contributors = []
+      
+      if current_user.roles.include?("site_admin") && current_user.sign_in_count == 2 && current_account.users.count == 1
+        redirect_to edit_account_setting_path(current_account)
+      end
   end
   
   def check_account
