ea75981255ed917ac2504cff994ef9c43ff5ec53
neutrino <ranendra@sprout-technology.com>
2010-10-30 09:39:11 +0545

    added a checkboxs that selects off days of a week

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -5,10 +5,16 @@ class SettingsController < ApplicationController
   
   def index
       @account = Account.find(current_account)
+      @days = Account.days.sort
   end
   
   def update
+
+    off_days = params[:office_closing_days]
+    
     @account = Account.find(params[:id])
+    @account.office_closing_days = off_days if ! off_days.nil?
+
     if @account.update_attributes(params[:account]) 
       redirect_to '/', :notice => 'Setting was successfully updated.'
     else
